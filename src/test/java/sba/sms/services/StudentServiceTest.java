28e993f5fa15a6be9acc6ce618f0e73b4c2b94bd
Nick Shore <NickShore111@gmail.com>
2022-05-20 01:47:36 -0700

    final completed

--- src/test/java/sba/sms/services/StudentServiceTest.java
+++ src/test/java/sba/sms/services/StudentServiceTest.java
@@ -10,13 +10,21 @@ import sba.sms.utils.CommandLine;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 class StudentServiceTest {
+    final List<Student> allValidStudents = new ArrayList<>(Arrays.asList(
 
+            new Student("reema@gmail.com", "reema brown", "password"),
+            new Student("annette@gmail.com", "annette allen", "password"),
+            new Student("anthony@gmail.com", "anthony gallegos", "password"),
+            new Student("ariadna@gmail.com", "ariadna ramirez", "password"),
+            new Student("bolaji@gmail.com", "bolaji saibu", "password")
+    ));
     static StudentService studentService;
 
     @BeforeAll
@@ -28,16 +36,7 @@ class StudentServiceTest {
     @Test
     @Order(1)
     void getAllStudents() {
-
-        List<Student> expected = new ArrayList<>(Arrays.asList(
-
-                new Student("reema@gmail.com", "reema brown", "password"),
-                new Student("annette@gmail.com", "annette allen", "password"),
-                new Student("anthony@gmail.com", "anthony gallegos", "password"),
-                new Student("ariadna@gmail.com", "ariadna ramirez", "password"),
-                new Student("bolaji@gmail.com", "bolaji saibu", "password")
-        ));
-        assertThat(studentService.getAllStudents()).hasSameElementsAs(expected);
+        assertThat(studentService.getAllStudents()).hasSameElementsAs(allValidStudents);
     }
 
     @Test
